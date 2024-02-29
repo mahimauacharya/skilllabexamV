52038b07eab8a0334108a78e6cb1fa42fbf1113e
mahimauacharya <mahimau579@gmail.com>
2024-02-29 16:50:24 +0530

    commit

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(bodyParser.json());
 
 // Connect to MongoDB (replace 'your-mongodb-url' with your actual MongoDB connection string)
 // mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connect('mongodb://127.0.0.1:27017/mahima', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/sip', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
